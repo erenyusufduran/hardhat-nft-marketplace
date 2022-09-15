adeef2ecf2f58efd76b8dcc36271035eb12cc970
Eren Yusuf Duran <erenyusufduran1905@gmail.com>
2022-09-15 13:45:07 +0300

    hardhat structure

---
+++ hardhat.config.js
@@ -0,0 +1,64 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-deploy");
+require("solidity-coverage");
+require("hardhat-gas-reporter");
+require("hardhat-contract-sizer");
+require("dotenv").config();
+
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL;
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      forking: {
+        url: MAINNET_RPC_URL,
+      },
+      blockConfirmations: 1,
+    },
+    rinkeby: {
+      chainId: 4,
+      blockConfirmations: 6,
+      url: RINKEBY_RPC_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+  gasReporter: {
+    enabled: false,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    coinmarketcap: COINMARKETCAP_API_KEY,
+    token: "ETH",
+  },
+  solidity: {
+    compilers: [
+      { version: "0.8.7" },
+      { version: "0.4.19" },
+      { version: "0.6.12" },
+      { version: "0.6.6" },
+      { version: "0.6.0" },
+    ],
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    users: {
+      default: 1,
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  mocha: {
+    timeout: 4000000,
+  },
+};
